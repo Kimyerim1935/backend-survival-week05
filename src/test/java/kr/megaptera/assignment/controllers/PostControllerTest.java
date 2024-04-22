37b039b0940f57ea58e89218204d827389e24a74
Kimyerim1935 <kimyerim1935@naver.com>
2024-04-22 22:28:48 +0900

    add: 5주차 과제

--- src/test/java/kr/megaptera/assignment/controllers/PostControllerTest.java
+++ src/test/java/kr/megaptera/assignment/controllers/PostControllerTest.java
@@ -1,9 +1,35 @@
 package kr.megaptera.assignment.controllers;
 
+import kr.megaptera.assignment.application.CreatePostService;
+import kr.megaptera.assignment.application.DeletePostService;
+import kr.megaptera.assignment.application.GetPostService;
+import kr.megaptera.assignment.application.GetPostsService;
+import kr.megaptera.assignment.application.UpdatePostService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 class PostControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    private GetPostsService getPostsService;
+
+    @MockBean
+    private GetPostService getPostService;
+
+    @MockBean
+    private CreatePostService createPostService;
+
+    @MockBean
+    private UpdatePostService updatePostService;
+
+    @MockBean
+    private DeletePostService deletePostService;
+
     @Test
     @DisplayName("GET /posts")
     void list() {
